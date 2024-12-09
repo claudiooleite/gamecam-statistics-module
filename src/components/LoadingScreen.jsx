60d24df5d96b04c2a2b5bb70ac2e888981cbe867
Claudio Leite <claudio.leite@hotmail.com>
2024-12-09 10:26:38 +0100

    logo added to loading

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,8 +1,10 @@
 import "../styles/loadingscreen.css";
+import image from '../assets/gamecam_logo_white.png'
 
 function LoadingScreen() {
     return (
         <div className="loader-main">
+            <img src={image} className="image-logo" alt="logo" />
             <div className="loader"></div>
         </div>
     );
